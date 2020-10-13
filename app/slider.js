9ba90706417ae7db1ff7b1d860c587d533ae6067
OlehTovkaniuk <tovkaniuk@gmail.com>
2020-10-13 17:41:16 +0300

    ready mobile layout

--- app/slider.js
+++ app/slider.js
@@ -1,14 +1,53 @@
 
-const slides = document.querySelectorAll('.slide-photo');
-const next = document.querySelector('#button-right');
-const prev = document.querySelector('#button-left');
-const auto = false;
-const interval = 5000;
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('#button-left');
+const nextBtn = document.querySelector('#button-right');
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
+
 
 const nextSlide = () => {
-    // get current class
     const current = document.querySelector('.current');
-    // remove current class
     current.classList.remove('current');
-    // 
+    if (current.nextElementSibling) {
+        current.nextElementSibling.classList.add('current');
+    } else {
+        slides[0].classList.add('current');
+    };
+}
+
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if (current.previousElementSibling) {
+        current.previousElementSibling.classList.add('current');
+    } else {
+        slides[slides.length - 1].classList.add('current');
+    }
+}
+
+// button events
+
+nextBtn.addEventListener('click', e => {
+    nextSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+
+prevBtn.addEventListener('click', e => {
+    prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+})
+
+//  Auto slide
+
+if (auto) {
+    // Run next slide at interval time
+    slideInterval = setInterval(nextSlide, intervalTime);
 }
